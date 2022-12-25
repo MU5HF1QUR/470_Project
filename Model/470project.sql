de2fc69dc4279ab6415a16190e924502d25aee30
Musfiqur Rahman Anik <31827927+MU5HF1QUR@users.noreply.github.com>
2022-12-25 22:04:19 +0600

    Update 470project.sql

--- Model/470project.sql
+++ Model/470project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 06, 2019 at 08:34 PM
+-- Generation Time: November 20, 2022 at 08:34 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
